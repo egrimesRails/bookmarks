3142b262c355027c2a7945caca92495423a4396a
grimesRails <egrimes@madisoncollege.edu>
2014-02-25 21:16:03 -0600

    halfway point project 1

---
+++ spec/models/bookmark_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Bookmark do
+  
+  before do
+    @bookmark = Bookmark.new(name: "Example User", url: "user@example.com", saved_at: "time")
+  end
+  
+  subject { @bookmark }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:url) }
+  
+  it { should be_valid }
+  
+  describe "when name is not present" do
+    before { @bookmark.name = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when url is not present" do
+    before { @bookmark.url = " " }
+    it { should_not be_valid }
+  end
+end
